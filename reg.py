6be338cac28082b6b69eee38f9e3f57292c0f5fd
fnnetwork <fnnetwork272@gmail.com>
2025-02-02 22:41:38 +0530

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,26 +1,31 @@
 import re
+
 def reg(cc):
-	regex = r'\d+'
-	matches = re.findall(regex, cc)
-	match = ''.join(matches)
-	n = match[:16]
-	mm = match[16:18]
-	yy = match[18:20]
-	if yy == '20':
-		yy = match[18:22]
-		if n.startswith("3"):
-			cvc = match[22:26]
-		else:
-			cvc = match[22:25]
-	else:
-		if n.startswith("3"):
-			cvc = match[20:24]
-		else:
-			cvc = match[20:23]
-	cc = f"{n}|{mm}|{yy}|{cvc}"
-	if not re.match(r'^\d{16}$', n):
-		return
-	if not re.match(r'^\d{3,4}$', cvc):
-		return
-	return cc
+    regex = r'\d+'
+    matches = re.findall(regex, cc)
+    match = ''.join(matches)
+
+    if len(match) < 16:
+        return None  # Not a valid CC input
+
+    n = match[:16]
+    mm = match[16:18]
+    yy = match[18:20]
+
+    if yy == '20':
+        yy = match[18:22]
+        cvc_start = 22
+    else:
+        cvc_start = 20
+
+    if n.startswith("3"):
+        cvc = match[cvc_start:cvc_start + 4]  # Amex requires 4-digit CVC
+    else:
+        cvc = match[cvc_start:cvc_start + 3]
+
+    # Validate extracted values
+    if not re.match(r'^\d{16}$', n) or not re.match(r'^\d{2}$', mm) or not re.match(r'^\d{2,4}$', yy) or not re.match(r'^\d{3,4}$', cvc):
+        return None
+
+    return f"{n}|{mm}|{yy}|{cvc}"
 	
